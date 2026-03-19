31955a6b5cd8a747e73e50b3101402334e1d7851
Lexskyld <haiwenshi68@gmail.com>
2026-03-19 01:59:42 +0000

    data collection of high and low drag

--- OrbitalDecay.java
+++ OrbitalDecay.java
@@ -5,8 +5,9 @@ public class OrbitalDecay{
     public static double R_Earth = 6371000;
 
 public static void main(String[]args){
+StringBuilder csvContent = new StringBuilder("Day,Altitude\n");
     double M_Satelite = 5.00;
-    double A_Satelite = 1;
+    double A_Satelite = 0.01;
     double C_Drag = 2.2;
     double Altitude = 400000;
     double R = R_Earth + Altitude;
@@ -29,9 +30,18 @@ while (Altitude > 150000){
     if (time % 86400 < DistanceTime){
         System.out.printf("| %-3.0f  | %-15.6f | %-16.2f | %-10.2e |\n",
                            time / 86400, Altitude / 1000, v , rho);
-    
+    csvContent.append(time/86400).append(",").append(Altitude/1000).append("\n");
+   
     }  
 }
+
+try {
+    java.nio.file.Files.write(java.nio.file.Paths.get("decay_data.csv"), csvContent.toString().getBytes());
+    System.out.println(">>>success! decay_data.csv");
+   } catch (java.io.IOException e) {
+   System.out.println("error has occurred " + e.getMessage());
+}
+   
 System.out.println("-----------------------------------------------------------------");
 System.out.printf("The projected life of orbit is : %.2f days\n" , time / 86400);
 }
@@ -40,11 +50,10 @@ public static double findDensity(double h) {
     double altKm = h/1000.0;
     if (altKm > 150) {
         return 2.789e-10 * Math.exp(-(altKm - 200)/ 37.5);
-    } 
+    }
     else {
         return 1.225 * Math.exp(-altKm / 8.5);
     }
 }
 }
-
 
